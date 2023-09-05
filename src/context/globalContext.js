9c44d1bc4de70c47ca2f1c7e318b8d286dda9bd2
Muhammad <muhammad.bw.03@gmail.com>
2023-09-05 10:21:49 +0500

    first commit

---
+++ src/context/globalContext.js
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from "react"
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:8080/api/v1/";
+const GlobalContext = React.createContext()
+
+export const GlobalProvider = ({ children }) => {
+
+    const [incomes, setIncomes] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+    const [error, setError] = useState(null);
+
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) => {
+                setError(err.response.data.message)
+            })
+    }
+
+
+    const getIncomes = async () => {
+        const response = await axios.get(`${BASE_URL}get-incomes`)
+        setIncomes(response.data);
+    }
+    return (
+        <GlobalContext.Provider value={{
+            addIncome,
+            getIncomes,
+            incomes
+        }}>
+            {children}
+        </GlobalContext.Provider >
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(GlobalContext)
+}
